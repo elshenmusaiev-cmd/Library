ffb756d89b91d229fb3e507089a13e26261d7eeb
musaev <musaevv.0001@gmail.com>
2026-03-23 18:01:17 +0200

    libray

---
+++ Library/Librarian.cs
@@ -0,0 +1,8 @@
+﻿public class Librarian
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public List<Issue> Issues { get; set; }
+}
+
---
+++ Library/Reader.cs
@@ -0,0 +1,7 @@
+﻿public class Reader
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public List<Issue> Issues { get; set; }
+}
